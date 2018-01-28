37d916cc26d37a4e4e898d3533251799301e906a
orions618belt <joel.redick18@gmail.com>
2018-01-28 16:21:00 -0500

    Added content files

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+// Navbar Change on Scroll
+window.onscroll = function navChange() {
+  var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-black", "");
+    }
+};
+
+// Mobile Navbar Toggle Menu
+function toggleFunction() {
+  var x = document.getElementById("mobileNav");
+  if (x.className.indexOf("w3-show") === -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
